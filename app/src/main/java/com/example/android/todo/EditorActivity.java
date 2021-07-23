60fb6eae62860f48188eaf27dcd6e5d85bebc105
nmnsharma007 <nmnsharma20@gmail.com>
2021-07-23 13:58:27 +0530

    Setting up editor activity

--- app/src/main/java/com/example/android/todo/EditorActivity.java
+++ app/src/main/java/com/example/android/todo/EditorActivity.java
@@ -3,13 +3,18 @@ package com.example.android.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-
+        // Find the toolbar view inside the activity layout
+        mToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
     }
 }
--- app/src/main/java/com/example/android/todo/ItemsActivity.java
+++ app/src/main/java/com/example/android/todo/ItemsActivity.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -19,11 +20,8 @@ import java.util.List;
 public class ItemsActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = ItemsActivity.class.getSimpleName();
-    // Recycler view for the main activity
-    RecyclerView recyclerView;
-    // member variable for the View Model
+    private RecyclerView mRecyclerView;
     private TaskViewModel mTaskViewModel;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +34,20 @@ public class ItemsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // open up an intent to go to the editor activity when the fab button is pressed
                 Intent intent = new Intent(ItemsActivity.this, EditorActivity.class);
+                intent.putExtra(getString(R.string.package_name)+"id",-1);
                 startActivity(intent);
             }
         });
         // lookup the recycler view in activity layout
-        recyclerView = findViewById(R.id.rvTasks);
+        mRecyclerView = findViewById(R.id.rvTasks);
         // associate the view model with the activity
         mTaskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
         mTaskViewModel.getAllTasks().observe(this, tasks -> setUpRecyclerView(tasks));
     }
 
     private void setUpRecyclerView(List<Task> tasks) {
-        final TaskAdapter taskAdapter = new TaskAdapter(null);
-        recyclerView.setAdapter(taskAdapter);
-        if(recyclerView == null) {
-            Log.v(LOG_TAG,"yes it is null");
-        }
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final TaskAdapter taskAdapter = new TaskAdapter(tasks);
+        mRecyclerView.setAdapter(taskAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
